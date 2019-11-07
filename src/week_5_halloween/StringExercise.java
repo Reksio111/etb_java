f7647242309b29540c5afd02d0ebb95121a7f407
TST03 <TST03@LYTC-TST03.TRAINING.LOCAL>
2019-11-07 13:43:55 +0000

    string exercise

---
+++ src/week_5_halloween/StringExercise.java
@@ -0,0 +1,12 @@
+package week_5_halloween;
+
+public class StringExercise {
+
+	public static void main(String[] args) {
+	
+		String str="Dublin is captitol of Republic of Ireland.";
+		System.out.print("Given sentence contains " + str.length() + " letters inluding gaps");
+
+	}
+
+}
