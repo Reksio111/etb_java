cf3e2ff00f9f7787a21fe4b09ee3c94f43492b24
TST03 <TST03@LYTC-TST03.TRAINING.LOCAL>
2019-11-11 11:48:50 +0000

    prime numbers class finished

--- src/week_6/Prime_Numbers.java
+++ src/week_6/Prime_Numbers.java
@@ -1,15 +1,43 @@
 package week_6;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Prime_Numbers {
 
 	public static void main(String[] args) {
 		Prime_Numbers tester=new Prime_Numbers();
 	
-		System.out.print(tester.isPrime(2));
+		int [] arr = new int[500];
+		for (int i = 0 ; i < arr.length; i++) 
+		arr[i]=i;	
+
+		
+	
+		System.out.println(tester.isPrime(6));
+		tester.isPrimeArray(arr);
+		
 	
 	}
+	
 	boolean isPrime(int n) {
-		
-		return true;
+	    //check if n is a multiple of 2
+	    if (n%2==0) 
+	    	return false;
+	    //if not, then just check the odds
+	    for(int i=3;i*i<=n;i+=2) {
+	        if(n%i==0)
+	            return false;
+	    }
+	    return true;
 	}
+	public void isPrimeArray( int[] arr) {
+	
+		for (int i = 0 ; i < arr.length; i++) {
+			if(isPrime(arr[i]))
+		System.out.print(arr[i] + ",");	
+		}
+		
+	}
+	
 }
