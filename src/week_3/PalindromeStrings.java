76351584347ddca552923f0c8a1ca99cf8142764
TST03 <TST03@LYTC-TST03.TRAINING.LOCAL>
2019-11-12 15:39:49 +0000

    Palindrome A palindrome is a word, phrase or sequence that reads the same backwards as forwards. Write a method isPalindrome thats accepts a String as a parameter and returns true if the String is a palindrome, false otherwise. Stage 1 (simple) Test your code with the following palindromes: Navan Madam Noon Civic Radar Level Rats live on no evil star
    Stage 2 (tricky)
    Test your code (and adapt it if necessary) with all of the following
    palindromes:
    Never odd or even
    Mr. Owl ate my metal worm
    A man, a plan, a canal – Panama
    Madam, I'm Adam
    Hint 1: Remember to break your code up into manageable methods which you
    can test independently
    Hint 2: Try coding the solution both with and without the use of regular
    expressionsPalindrome

---
+++ src/week_3/PalindromeStrings.java
@@ -0,0 +1,34 @@
+package week_3;
+/*
+ * Palindrome
+A palindrome is a word, phrase or sequence that reads the same backwards as forwards.
+Write a method isPalindrome thats accepts a String as a parameter and returns true if the String is a palindrome, false otherwise.
+Stage 1 (simple)
+Test your code with the following palindromes:
+Navan
+Madam
+Noon
+Civic
+Radar
+Level
+Rats live on no evil star
+
+Stage 2 (tricky)
+Test your code (and adapt it if necessary) with all of the following palindromes:
+Never odd or even
+Mr. Owl ate my metal worm
+A man, a plan, a canal – Panama
+Madam, I'm Adam
+Hint 1: Remember to break your code up into manageable methods which you can test independently
+Hint 2: Try coding the solution both with and without the use of regular expressions.
+
+ * */
+
+public class PalindromeStrings {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
